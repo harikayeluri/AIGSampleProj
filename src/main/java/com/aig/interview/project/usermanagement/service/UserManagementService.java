93948b99feb1b62a37bc3ee6da8c4ed1254cae47
harikayeluri <34264635+harikayeluri@users.noreply.github.com>
2019-05-20 20:08:36 +0530

    Update UserManagementService.java

--- src/main/java/com/aig/interview/project/usermanagement/service/UserManagementService.java
+++ src/main/java/com/aig/interview/project/usermanagement/service/UserManagementService.java
@@ -1,11 +1,10 @@
 package com.aig.interview.project.usermanagement.service;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.exception.DataException; 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,36 +17,36 @@ import com.aig.interview.project.usermanagement.model.User;
 @Service
 public class UserManagementService {
 	
+	private static final Logger log = LoggerFactory.getLogger(UserManagementService.class);
 	@Autowired
 	UserManagementRepo userRepo;
 	
-
-	/*private List<User> users= new ArrayList(
-			Arrays.asList(new User((long) 1001,"Harika","password1"),
-					new User((long) 1002,"Rita","password2")))
-		;*/
-
-	public Optional<User> getUserbyId(Long userId) throws SQLException {
+	
+	public Optional<User> getUserbyId(Long userId)  {		
+	
+		return userRepo.findById(userId);
 		
-	return userRepo.findById(userId);
 		
-		// return users.stream().filter(s->s.getUUID().equals(userId)).findFirst().get();
 	}
 
-	public Page<User> getUsers(Pageable pageable) throws SQLException {
+	public Page<User> getUsers(Pageable pageable)  {
 		
 		return userRepo.findAll(pageable);
 		
-		// return users;
+		
 	}
 
-	public void addUser(User user)throws SQLException {
-		 userRepo.save(user);
-		// users.add(user) ;
+	public void addUser(User user){
+		try
+		{userRepo.save(user);
+		}
+		catch(DataException e) {
+			log.error("SQL Exception=", e);
+		
 	}
 	
 	
-	
+	}
 	
 	
 	
