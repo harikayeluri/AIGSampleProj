ab98b0e7176e68c57340b3a474a2415dfa657518
sss <satyaharika.y@gmail.com>
2019-05-20 19:25:31 +0530

    to master

--- src/main/java/com/aig/interview/project/usermanagement/Repository/UserManagementRepo.java
+++ src/main/java/com/aig/interview/project/usermanagement/Repository/UserManagementRepo.java
@@ -1,16 +1,15 @@
 package com.aig.interview.project.usermanagement.Repository;
 
+import org.hibernate.exception.DataException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
 import org.springframework.stereotype.Repository;
 
 import com.aig.interview.project.usermanagement.model.User;
 @Repository
 public interface UserManagementRepo extends JpaRepository<User,Long> {
 	
-	 Page<User> findAll(Pageable pageable);
+	 Page<User> findAll(Pageable pageable) throws DataException ;
 
 }
--- src/main/java/com/aig/interview/project/usermanagement/service/UserManagementService.java
+++ src/main/java/com/aig/interview/project/usermanagement/service/UserManagementService.java
@@ -6,7 +6,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.exception.DataException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -18,36 +22,36 @@ import com.aig.interview.project.usermanagement.model.User;
 @Service
 public class UserManagementService {
 	
+	private static final Logger log = LoggerFactory.getLogger(UserManagementService.class);
 	@Autowired
 	UserManagementRepo userRepo;
 	
-
-	/*private List<User> users= new ArrayList(
-			Arrays.asList(new User((long) 1001,"Harika","password1"),
-					new User((long) 1002,"Rita","password2")))
-		;*/
-
-	public Optional<User> getUserbyId(Long userId) throws SQLException {
+	
+	public Optional<User> getUserbyId(Long userId)  {		
+	
+		return userRepo.findById(userId);
 		
-	return userRepo.findById(userId);
 		
-		// return users.stream().filter(s->s.getUUID().equals(userId)).findFirst().get();
 	}
 
-	public Page<User> getUsers(Pageable pageable) throws SQLException {
+	public Page<User> getUsers(Pageable pageable)  {
 		
 		return userRepo.findAll(pageable);
 		
-		// return users;
+		
 	}
 
-	public void addUser(User user)throws SQLException {
-		 userRepo.save(user);
-		// users.add(user) ;
+	public void addUser(User user){
+		try
+		{userRepo.save(user);
+		}
+		catch(DataException e) {
+			log.error("SQL Exception=", e);
+		
 	}
 	
 	
-	
+	}
 	
 	
 	
